1d479cffc9c3ea8c3ed50a5a61f5c827fa8879e7
shibli049 <shibli05@gmail.com>
2015-12-17 13:21:27 +0600

    added desktop notification for tasks

--- contact-list.js
+++ contact-list.js
@@ -1,4 +1,39 @@
 Tasks = new Mongo.Collection("tasks");
+// var my_notification = null;
+
+
+function notifyMe(msg) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    // Meteoris.Flash.set('denied', msg);
+    return;
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var my_notification = new Notification(msg,
+      {
+        // body: '',
+        tag: 'default'
+      }
+    );
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+         var my_notification = new Notification(msg,{tag: 'default'});
+       }
+      });
+    }
+
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
 
 if (Meteor.isClient) {
   // This code only runs on the client
@@ -25,6 +60,8 @@ if (Meteor.isClient) {
 
       // Clear form
       event.target.text.value = "";
+      notifyMe('Task:' + text);
+
     }
   });
 
@@ -34,9 +71,18 @@ if (Meteor.isClient) {
       Tasks.update(this._id, {
         $set: {checked: ! this.checked}
       });
+
+      notifyMe('Done:'+ this.text);
+
     },
     "click .delete": function () {
       Tasks.remove(this._id);
+      notifyMe('Deleted:' + this.text);
     }
+
+
+
   });
+
+
 }
